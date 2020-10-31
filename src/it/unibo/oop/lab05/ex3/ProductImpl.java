4939925e9865b85ca03c87dc64bf07ffc846acce
Rostic <rosticdovga@gmail.com>
2020-10-31 11:50:13 +0100

    Implementazione di ProductImpl

--- src/it/unibo/oop/lab05/ex3/ProductImpl.java
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -2,16 +2,24 @@ package it.unibo.oop.lab05.ex3;
 
 public class ProductImpl implements Product {
 
+	private String name;
+	private double quantity;
+	
+	public ProductImpl(final String name, final double quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
 	@Override
 	public double getQuantity() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.quantity;
 	}
 
 }
