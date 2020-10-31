828d5c816ef498cec55c9e79f70eaeec63f7f335
Rostic <rosticdovga@gmail.com>
2020-10-31 12:06:17 +0100

    UseWarehouse e WarehouseImpl implementate e funzionanti

--- src/it/unibo/oop/lab05/ex3/UseWarehouse.java
+++ src/it/unibo/oop/lab05/ex3/UseWarehouse.java
@@ -27,11 +27,11 @@ public final class UseWarehouse {
          * them to run the test.
          */
 
-        final Product p1 = null;
-        final Product p2 = null;
-        final Product p3 = null;
+        final Product p1 = new ProductImpl("Mele", 55.3);
+        final Product p2 = new ProductImpl("Pere", 20.7);
+        final Product p3 = new ProductImpl("Melanzane", 15.5);
 
-        final Warehouse w = null;
+        final Warehouse w = new WharehouseImpl();
         w.addProduct(p3);
         w.addProduct(p1);
         w.addProduct(p2);
--- src/it/unibo/oop/lab05/ex3/WharehouseImpl.java
+++ src/it/unibo/oop/lab05/ex3/WharehouseImpl.java
@@ -1,36 +1,45 @@
 package it.unibo.oop.lab05.ex3;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class WharehouseImpl implements Warehouse {
 
+	final private Set<Product> set = new LinkedHashSet<Product>();
+	
 	@Override
 	public void addProduct(Product p) {
-		// TODO Auto-generated method stub
-
+		this.set.add(p);
 	}
 
 	@Override
 	public Set<String> allNames() {
-		// TODO Auto-generated method stub
-		return null;
+		final var nomi = new TreeSet<String>();
+		for(Product p: set) {
+			nomi.add(p.getName());
+		}
+		return nomi;
 	}
 
 	@Override
 	public Set<Product> allProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		return new HashSet<>(this.set);
 	}
 
 	@Override
 	public boolean containsProduct(Product p) {
-		// TODO Auto-generated method stub
-		return false;
+		return set.contains(p);
 	}
 
 	@Override
 	public double getQuantity(String name) {
-		// TODO Auto-generated method stub
+		for(Product p: set) {
+			if(p.getName() == name) {
+				return p.getQuantity();
+			}
+		}
 		return 0;
 	}
 
