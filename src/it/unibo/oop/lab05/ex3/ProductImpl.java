abea3818d0de385371e1d939cd3243b5d8d852cc
Rostic <rosticdovga@gmail.com>
2020-10-31 11:39:41 +0100

    (Ex 3) Add Classi che implementano le interfacce Product e Warehouse

---
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -0,0 +1,17 @@
+package it.unibo.oop.lab05.ex3;
+
+public class ProductImpl implements Product {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getQuantity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/it/unibo/oop/lab05/ex3/WharehouseImpl.java
@@ -0,0 +1,37 @@
+package it.unibo.oop.lab05.ex3;
+
+import java.util.Set;
+
+public class WharehouseImpl implements Warehouse {
+
+	@Override
+	public void addProduct(Product p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Set<String> allNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Product> allProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean containsProduct(Product p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getQuantity(String name) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
